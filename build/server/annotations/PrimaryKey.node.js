89eeb26b62369693c781729a565bdcbb9114457d
Anthony Cleaver <anthony.cleaver@midlandhr.co.uk>
2016-07-04 14:11:58 +0100

    Removing builds from repo

--- build/server/annotations/PrimaryKey.node.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-var DecorateField_node_1 = require('../services/DecorateField.node');
-function PrimaryKey(target, key) {
-    'use strict';
-    DecorateField_node_1.DecorateField.setPrimaryKey(target, key);
-}
-exports.PrimaryKey = PrimaryKey;
