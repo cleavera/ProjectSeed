85a0ddc4b0a6c35a36d228d4f97d9a058fd32306
Anthony Cleaver <anthony.cleaver@midlandhr.co.uk>
2016-07-06 11:10:19 +0100

    Resources return hypermedia links for child resources

--- src/domain/annotations/Child.node.ts
+++ src/domain/annotations/Child.node.ts
@@ -1,9 +1,28 @@
+import {IRoutingContext} from '../interfaces/IRoutingContext';
+import {Context} from '../classes/Context.node';
+
 export function Child(childModel: any, alias: string): ClassDecorator {
     'use strict';
 
     return function(model: any): void {
         if (!model._children) {
             model._children = {};
+
+            let oldFunction: (context: IRoutingContext) => any = model.prototype.generateLinks;
+
+            model.prototype.generateLinks = function(context: IRoutingContext): any {
+                let links: any = oldFunction.apply(this, arguments);
+
+                Object.keys(model._children).forEach(child => {
+                    let childContext: IRoutingContext = new Context(child, null, model._children[child], null, context);
+
+                    links[child] = {
+                        href: childContext.generateUrl()
+                    };
+                });
+
+                return links;
+            };
         }
 
         model._children[alias.toLowerCase()] = childModel;
