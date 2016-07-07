3fb6d43a2a0a48582ec4207c99cb519a69aeff2d
Anthony Cleaver <anthony.cleaver@midlandhr.co.uk>
2016-07-07 14:10:44 +0100

    Adding interfaces package

---
+++ src/packages/Interfaces.ts
@@ -0,0 +1,31 @@
+import {IData} from '../interfaces/IData';
+import {IIteratorResult} from '../interfaces/IIteratorResult';
+import {IModel} from '../interfaces/IModel';
+import {IRequest} from '../interfaces/IRequest';
+import {IResource} from '../interfaces/IResource';
+import {IResponse} from '../interfaces/IResponse';
+import {IRest} from '../interfaces/IRest';
+import {IRoot} from '../interfaces/IRoot';
+import {IRouter} from '../interfaces/IRouter';
+import {IRoutingContext} from '../interfaces/IRoutingContext';
+import {ISerialisable} from '../interfaces/ISerialisable';
+import {IServerError} from '../interfaces/IServerError';
+import {IUrl} from '../interfaces/IUrl';
+import {IValidator} from '../interfaces/IValidator';
+
+export {
+    IData,
+    IIteratorResult,
+    IModel,
+    IRequest,
+    IResource,
+    IResponse,
+    IRest,
+    IRoot,
+    IRouter,
+    IRoutingContext,
+    ISerialisable,
+    IServerError,
+    IUrl,
+    IValidator
+}
