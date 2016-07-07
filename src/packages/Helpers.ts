ce6e94dd2372c9da34b7db59a015b5744402904f
Anthony Cleaver <anthony.cleaver@midlandhr.co.uk>
2016-07-07 14:45:28 +0100

    Creating helpers package

---
+++ src/packages/Helpers.ts
@@ -0,0 +1,13 @@
+import {DecorateField} from '../services/DecorateField';
+import {Guid} from '../services/Guid';
+import {Log} from '../services/Log';
+import {noop} from '../services/noop';
+import {Transformer} from '../services/Transformer';
+
+export {
+    DecorateField,
+    Guid,
+    Log,
+    noop,
+    Transformer
+}
